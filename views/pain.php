2fa8fa3b9700da8511ddf9fc76bba0ba864be742
heng michel <cavacmoi@gmail.com>
2023-11-20 16:40:00 +0100

    modif7

--- views/pain.php
+++ views/pain.php
@@ -4,30 +4,128 @@ require_once "inc/nav.php";
 ?>
 
 <div class="container">
-    <h1 class="pain fw-semibold mt-5 text-center">Notre gamme de pain</h1>
-
-    <div id="carouselExampleFade" class="carousel slide carousel-fade mt-5">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="asset/photos/pains/1699988450733.jpg" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="asset/photos/pains/1699988450740.jpg" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="asset/photos/pains/1699988450747.jpg" class="d-block w-100" alt="...">
-          </div>
+  <h1 class="pain fw-semibold mt-5 text-center">Notre gamme de pain</h1>
+  <h3 class="baguette fw-semibold mt-5 text-center">nos baguettes</h3>
+  <div id="carouselExampleCaptions" class="carousel slide mt-5">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="asset/photos/pains/laTradition.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Tradition</h5>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
       </div>
-
+      <div class="carousel-item">
+        <img src="asset/photos/pains/laTradionCereale.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Tradition céréale</h5>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="asset/photos/pains/banepi.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Banépi</h5>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="asset/photos/pains/classique.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Classique</h5>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon bg-primary" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon bg-primary" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <h3 class="pains fw-semibold mt-5 text-center">nos pains</h3>
+  <div id="carouselExampleCaptions2" class="carousel slide mt-5">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="3" aria-label="Slide 4"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="4" aria-label="Slide 5"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="5" aria-label="Slide 6"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="6" aria-label="Slide 7"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="7" aria-label="Slide 8"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="8" aria-label="Slide 9"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="asset/photos/pains/laBouleAlAncienne.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Boule à l'ancienne</h5>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="asset/photos/pains/aLancienne.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>à l'ancienne</h5>
+        </div>
+      </div>  
+      <div class="carousel-item">
+        <img src="asset/photos/pains/complet.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Complet</h5>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="asset/photos/pains/seigle.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Seigle</h5>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="asset/photos/pains/sesame.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Sésame</h5>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="asset/photos/pains/cereale.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Céréale</h5>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="asset/photos/pains/multigraine.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Multi-graine</h5>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="asset/photos/pains/bouleRaisin.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Boule aux raisins</h5>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="asset/photos/pains/bouleNoix.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+          <h5>Boule aux noix</h5>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon bg-primary" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="next">
+      <span class="carousel-control-next-icon bg-primary" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 </div>
 <?php
 include_once "inc/footer.php";
---
+++ views/viennoiserie.php
@@ -0,0 +1,151 @@
+<?php
+include_once "inc/header.php";
+require_once "inc/nav.php";
+?>
+
+<div class="container">
+    <h1 class="viennoiserie fw-semibold mt-5 text-center">Notre gamme de viennoiserie</h1>
+    <div id="carouselExampleCaptions2" class="carousel slide mt-5">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="5" aria-label="Slide 6"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="6" aria-label="Slide 7"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="7" aria-label="Slide 8"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="8" aria-label="Slide 9"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="9" aria-label="Slide 10"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="10" aria-label="Slide 11"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="11" aria-label="Slide 12"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="12" aria-label="Slide 13"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="13" aria-label="Slide 14"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="14" aria-label="Slide 15"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="15" aria-label="Slide 16"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="16" aria-label="Slide 17"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="17" aria-label="Slide 18"></button>
+        </div>
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="asset/photos/viennoiserie/vitrineViennoiserie.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Notre vitrine</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/croissantBeurre.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Croissant au beurre</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/painAuChoco.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Pain au chocolat</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/croissantNature.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Croissant nature</h5>
+          </div>
+        </div>  
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/chaussonAuxPommes.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Chausson aux pommes</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/painAuxRaisins.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Pain aux raisins</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/pepiteChocolat.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Pépite chocolat</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/escargotPistChoc.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Escargot pistache chocolat</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/briocheAuSucre.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Brioche au sucre</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/viennoisChocolat.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Viennois chocolat</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/chouquette.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Chouquette</h5>
+          </div>
+        </div>
+
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/painDemie.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Pain demie</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/beignetPommeNatureChocolatCreme.jpg" class="d-block w-100" alt="...">
+          <div class="beignet carousel-caption d-md-block form-select-lg sticky-top col-4 container">
+            <h5>Beignet nature/crême/pomme/chocolat</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/painAuLait.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Pain au lait</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/croissantAuxAmandes.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Croissant aux amandes</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/painAuChocolatAuxAmandes.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Pain au chocolat amande</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/miniViennoiserie.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Nos mini viennoiseries</h5>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="asset/photos/viennoiserie/viennoiseChocolatNature.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+            <h5>Viennoise nature et chocolat</h5>
+          </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon bg-primary" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="next">
+      <span class="carousel-control-next-icon bg-primary" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+</div>
+<?php
+include_once "inc/footer.php";
+?>
