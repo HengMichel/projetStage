1518bf12fbae8527c8cf70f4da00165dc20c70b1
heng michel <cavacmoi@gmail.com>
2023-11-20 16:54:57 +0100

    needCheckCodeViennois

--- views/viennoiserie.php
+++ views/viennoiserie.php
@@ -5,6 +5,49 @@ require_once "inc/nav.php";
 
 <div class="container">
     <h1 class="viennoiserie fw-semibold mt-5 text-center">Notre gamme de viennoiserie</h1>
+    <div id="carouselExampleCaptions" class="carousel slide mt-5">
+
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="asset/photos/pains/laTradition.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+              <h5>Tradition</h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="asset/photos/pains/laTradionCereale.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+              <h5>Tradition céréale</h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="asset/photos/pains/banepi.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+              <h5>Banépi</h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="asset/photos/pains/classique.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-md-block form-select-lg sticky-top col-3 container">
+              <h5>Classique</h5>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon bg-primary" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon bg-primary" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+
     <div id="carouselExampleCaptions2" class="carousel slide mt-5">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
