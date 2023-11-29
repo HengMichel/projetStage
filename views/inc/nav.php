39b8e4e687d2aad0e865754c5ff19153aee32053
heng michel <cavacmoi@gmail.com>
2023-11-29 10:24:11 +0100

    madiaQuerryUpdate2

--- views/inc/nav.php
+++ views/inc/nav.php
@@ -2,7 +2,7 @@
   <div class="btn-group" role="group">
     <button id="btnGroupDrop1" type="button" class="btn btn-outline-dark border-2 m-2 dropdown-toggle" title="Menu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
     <div class="dropdown-menu bg-secondary" aria-labelledby="btnGroupDrop1">
-      <a class="dropdown-item" href="http://localhost/projetStage/accueil.php">Acceuil</a>
+      <a class="dropdown-item" href="http://localhost/projetStage/acceuil.php">Acceuil</a>
       <a class="dropdown-item" href="http://localhost/projetStage/pain.php">Notre gamme de pain</a>
       <a class="dropdown-item" href="http://localhost/projetStage/viennoiserie.php">Notre gamme de viennoiserie</a>
       <a class="dropdown-item" href="http://localhost/projetStage/patisserie.php">Notre gamme de pâtisserie</a>
