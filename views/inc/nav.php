2c667322572d7586b28bc13d61a64a23e3ec416a
heng michel <cavacmoi@gmail.com>
2024-02-01 09:47:05 +0100

    htaccess for url ok

--- views/inc/nav.php
+++ views/inc/nav.php
@@ -2,12 +2,12 @@
   <div class="btn-group" role="group">
     <button id="btnGroupDrop1" type="button" class="btn btn-outline-dark border-2 m-2 dropdown-toggle" title="Menu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
     <div class="dropdown-menu bg-secondary" aria-labelledby="btnGroupDrop1">
-      <a class="dropdown-item" href="http://localhost/projetStage/accueil.php">Acceuil</a>
-      <a class="dropdown-item" href="http://localhost/projetStage/pain.php">Notre gamme de pain</a>
-      <a class="dropdown-item" href="http://localhost/projetStage/viennoiserie.php">Notre gamme de viennoiserie</a>
-      <a class="dropdown-item" href="http://localhost/projetStage/patisserie.php">Notre gamme de pâtisserie</a>
-      <a class="dropdown-item" href="http://localhost/projetStage/cuisine.php">Notre gamme de cuisine</a>
-      <a class="dropdown-item" href="http://localhost/projetStage/espaceCafe.php">Notre espace café</a>
+      <a class="dropdown-item" href="http://localhost/projetStage/accueil">Acceuil</a>
+      <a class="dropdown-item" href="http://localhost/projetStage/pain">Notre gamme de pain</a>
+      <a class="dropdown-item" href="http://localhost/projetStage/viennoiserie">Notre gamme de viennoiserie</a>
+      <a class="dropdown-item" href="http://localhost/projetStage/patisserie">Notre gamme de pâtisserie</a>
+      <a class="dropdown-item" href="http://localhost/projetStage/cuisine">Notre gamme de cuisine</a>
+      <a class="dropdown-item" href="http://localhost/projetStage/espaceCafe">Notre espace café</a>
     </div>
   </div>
 </div>
