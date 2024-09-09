8915ffe9f0a004be8017fb18c5fb0e396867ccde
HengMichel <cavacmoi@gmail.com>
2024-09-09 12:25:40 +0200

    update

--- views/inc/nav.php
+++ views/inc/nav.php
@@ -2,7 +2,7 @@
   <div class="btn-group" role="group">
     <button id="btnGroupDrop1" type="button" class="btn btn-outline-dark border-2 m-2 dropdown-toggle" title="Menu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
     <div class="dropdown-menu bg-secondary" aria-labelledby="btnGroupDrop1">
-      <a class="dropdown-item" href="http://localhost/projetStage/accueil">Acceuil</a>
+      <a class="dropdown-item" href="http://localhost/projetStage/accueil">Accueil</a>
       <a class="dropdown-item" href="http://localhost/projetStage/pain">Notre gamme de pain</a>
       <a class="dropdown-item" href="http://localhost/projetStage/viennoiserie">Notre gamme de viennoiserie</a>
       <a class="dropdown-item" href="http://localhost/projetStage/patisserie">Notre gamme de pâtisserie</a>
