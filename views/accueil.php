0e2dd19fde7e683842643103211403951934705d
HengMichel <cavacmoi@gmail.com>
2024-11-06 10:00:01 +0100

    modif picture accueil

--- views/accueil.php
+++ views/accueil.php
@@ -10,10 +10,10 @@ include_once "inc/nav.php";
 <div id="michelCarousel" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="asset/photos/newNight2.jpg" class="d-block w-100" alt="..." />
+      <img src="asset/photos/newNight2.jpg" class="d-block w-75" alt="..." />
     </div>
     <div class="carousel-item">
-      <img src="asset/photos/newDay2.jpg" class="d-block w-100" alt="..." />
+      <img src="asset/photos/newDay2.jpg" class="d-block w-75" alt="..." />
     </div>
   </div>
 </div>
